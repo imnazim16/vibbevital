a23a8bf7372788a1099f1f6674dfe763f993b774
nazim-frontend <frontend@gutentech.com>
2025-10-26 23:30:58 +0530

    new website of vibbevital

---
+++ assets/js/app.js
@@ -0,0 +1,46 @@
+const buttons = document.querySelectorAll(".btn");
+const items = document.querySelectorAll(".item");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    // remove 'active' from all buttons
+    buttons.forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    const filter = btn.getAttribute("data-filter");
+
+    items.forEach((item) => {
+      if (filter === "all" || item.classList.contains(filter)) {
+        item.classList.remove("hide");
+      } else {
+        item.classList.add("hide");
+      }
+    });
+  });
+});
+
+// fuulyear
+const YearText = document.getElementById("year");
+const year = new Date();
+YearText.innerText = year.getFullYear();
+
+// sticky header
+const headerSticky = document.querySelector(".header-section");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 80) {
+    headerSticky.classList.add("sticky");
+  } else {
+    headerSticky.classList.remove("sticky");
+  }
+});
+
+// faq
+const faqList = document.querySelectorAll(".faq");
+
+faqList.forEach((item) => {
+  const btn = item.querySelector("h4.title");
+  btn.addEventListener("click", (e) => {
+    faqList.forEach((faq) => faq.classList.remove("active"));
+    item.classList.toggle("active");
+  });
+});
